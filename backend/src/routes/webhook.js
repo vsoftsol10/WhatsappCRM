f171b1712395e8d0961c571e4a42a651298811c6
VsoftTraining <vsoftdevtraining@gmail.com>
2026-07-22 15:36:02 +0530

    webhook.js file second time updated

--- backend/src/routes/webhook.js
+++ backend/src/routes/webhook.js
@@ -22,10 +22,19 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    console.log("====================================");
-    console.log("WHATSAPP WEBHOOK RECEIVED");
-    console.log(JSON.stringify(req.body, null, 2));
-    console.log("====================================");
+
+    const value = req.body.entry?.[0]?.changes?.[0]?.value;
+    const message = value?.messages?.[0];
+
+    if (message) {
+
+        const phone = message.from;
+        const text = message.text?.body;
+
+        console.log("Phone :", phone);
+        console.log("Message :", text);
+
+    }
 
     res.sendStatus(200);
 });
